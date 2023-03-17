9f0bcfb8e174ab77745cafb5e5b506355fd7aa16
Boukary OUEDRAOGO <ouedraogo.boukary.bo@gmail.com>
2023-03-17 13:22:09 +0100

    installation des packages

--- R/build2.R
+++ R/build2.R
@@ -1,2 +1,5 @@
 # An optional custom script to run after Hugo builds your site.
 # You can delete it if you do not need it.
+list.of.packages <- c("ggplot2", "Rcpp",'tidyverse','flexdashboard','blogdown','rmarkdown')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
